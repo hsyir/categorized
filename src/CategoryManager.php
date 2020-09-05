cb78f295106c565e4582d76edecdeb2eaf1695d1
Hossein <hosseinyaghmaee@gmail.com>
2020-09-05 14:10:44 +0430

    Update CategoryManager.php
    add InitAndManageRoots to CategoryManager

--- src/CategoryManager.php
+++ src/CategoryManager.php
@@ -3,9 +3,10 @@
 namespace Hsy\Categorize;
 
 use Hsy\Categorize\Traits\CategorizeCache;
+use Hsy\Categorize\Traits\InitAndManageRoots;
 use Hsy\Categorize\Traits\RetrieveCategorizeDescendants;
 
 class CategoryManager
 {
-    use CategorizeCache, RetrieveCategorizeDescendants;
+    use CategorizeCache, RetrieveCategorizeDescendants,InitAndManageRoots;
 }
